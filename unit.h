b05b6b2f0d7743fa1b725a5d905145095c1492c1
mju-oss-13-b <mjuoss13b@gmail.com>
2013-11-19 23:48:02 +0900

    Update unit.h

--- unit.h
+++ unit.h
@@ -10,8 +10,8 @@ public:
         units();
         bool deleteNum(int num);
         bool textEnableNum1();
-        bool findNum(int num); // 返回该空格是否可以填这个数
-        int findToPush(); // 当空格只剩一个可填数时
+        bool findNum(int num); 
+        int findToPush(); 
         void clearEnableNum();
 private:
         int enableNum;
