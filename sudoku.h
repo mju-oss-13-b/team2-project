9e018ce90c6c082a0304e5ec6806f27481d7f7b3
asdfzxcooo <asdfzxcooo@naver.com>
2013-12-12 01:56:36 +0900

    Update sudoku.h

--- sudoku.h
+++ sudoku.h
@@ -1,14 +1,19 @@
 #include"unit.h"
 #include<iostream>
+#include<windows.h>
 #include<vector>
 #include<list>
+using namespace std;
+
 
 class sudoku
 {
 public:
         sudoku();
+		void gotoxy(int x, int y);
+		void print_main();
         bool setNum(int row, int col, int num);
-        bool disEnable(int row, int col, int num);
+	    bool disEnable(int row, int col, int num);
         void textCount();
         void testRowCount(int num, int row);
         void testColCount(int num, int col);
@@ -17,6 +22,7 @@ public:
         void setAdd();
         bool trySetAdd();
         bool isProgramOver();
+        bool trial( int id );
 private:
         int final[9][9];
         int rowCount[10][9];
@@ -25,3 +31,4 @@ private:
         units u[9][9];
         list<waitUnit> add;
 };
+
